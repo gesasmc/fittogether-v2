90acaf4968d1f04472a12991c051f700472743b9
gesasmc <pz9v7b4rwr@privaterelay.appleid.com>
2026-09-03 21:04:06 +0200

    Load V2.0.20 single-exercise start flow

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './v218.js'
 import './v219.js'
+import './v220.js'
 import App from './App.jsx'
 import './styles.css'
 import './v204.css'
@@ -12,6 +13,7 @@ import './v210.css'
 import './v211.css'
 import './v216.css'
 import './v217.css'
+import './v220.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
