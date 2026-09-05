c894056c7f5bc896a985b9d9e77fcb370d2f397f
gesasmc <pz9v7b4rwr@privaterelay.appleid.com>
2026-09-05 12:48:47 +0200

    Activate V2.0.49 custom plan builder

--- src/main.jsx
+++ src/main.jsx
@@ -25,6 +25,7 @@ import './v241.js'
 import './v242.js'
 import './v246.js'
 import './v247.js'
+import './v249.js'
 import App from './App.jsx'
 import AuthGate from './AuthGate.jsx'
 import './styles.css'
@@ -53,6 +54,7 @@ import './v241.css'
 import './v242.css'
 import './v246.css'
 import './v247.css'
+import './v249.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
