18ee55d52b58d657d0cace2178b0663dcd68ee45
gesasmc <pz9v7b4rwr@privaterelay.appleid.com>
2026-09-05 13:13:20 +0200

    Activate V2.0.53 Smart Trainer equipment logic

--- src/main.jsx
+++ src/main.jsx
@@ -27,6 +27,7 @@ import './v246.js'
 import './v247.js'
 import './v249.js'
 import './v251.js'
+import './v253.js'
 import App from './App.jsx'
 import AuthGate from './AuthGate.jsx'
 import './styles.css'
@@ -57,6 +58,7 @@ import './v246.css'
 import './v247.css'
 import './v249.css'
 import './v251.css'
+import './v253.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
