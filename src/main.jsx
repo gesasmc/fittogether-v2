f2ce7dff410f0b4bd718c119019ac4624566b1c4
gesasmc <pz9v7b4rwr@privaterelay.appleid.com>
2026-09-06 20:42:51 +0200

    Enable exercise detail history

--- src/main.jsx
+++ src/main.jsx
@@ -46,6 +46,7 @@ import './v289.js'
 import './v290.js'
 import './v291.js'
 import './v292.js'
+import './v293.js'
 import App from './App.jsx'
 import AuthGate from './AuthGate.jsx'
 import './styles.css'
@@ -96,6 +97,7 @@ import './v289.css'
 import './v290.css'
 import './v291.css'
 import './v292.css'
+import './v293.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><AuthGate><App /></AuthGate></React.StrictMode>,
