4876e9308195955bb39bf14b32bc15a5483f4de0
gesasmc <pz9v7b4rwr@privaterelay.appleid.com>
2026-09-06 15:19:03 +0200

    Load V2.0.75 set planning

--- src/main.jsx
+++ src/main.jsx
@@ -34,6 +34,7 @@ import './v254.js'
 import './v255.js'
 import './v256.js'
 import './v273.js'
+import './v275.js'
 import App from './App.jsx'
 import AuthGate from './AuthGate.jsx'
 import './styles.css'
@@ -70,6 +71,7 @@ import './v256.css'
 import './v270.css'
 import './v271.css'
 import './v273.css'
+import './v275.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
