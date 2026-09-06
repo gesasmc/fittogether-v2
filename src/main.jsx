d96a9b920432122fcb8acd34bfe770b34098e503
gesasmc <pz9v7b4rwr@privaterelay.appleid.com>
2026-09-06 13:11:50 +0200

    Load V2.0.70 pause control styles

--- src/main.jsx
+++ src/main.jsx
@@ -66,6 +66,7 @@ import './v251.css'
 import './v253.css'
 import './v255.css'
 import './v256.css'
+import './v270.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
