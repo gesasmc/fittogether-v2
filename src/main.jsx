60198d5bf6c7ec96472ba85a713b3be167b7e459
gesasmc <pz9v7b4rwr@privaterelay.appleid.com>
2026-09-06 18:29:26 +0200

    Enable Training UX V2

--- src/main.jsx
+++ src/main.jsx
@@ -40,6 +40,7 @@ import './v280.js'
 import './v281.js'
 import './v282.js'
 import './v283.js'
+import './v284.js'
 import App from './App.jsx'
 import AuthGate from './AuthGate.jsx'
 import './styles.css'
@@ -83,6 +84,7 @@ import './v280.css'
 import './v281.css'
 import './v282.css'
 import './v283.css'
+import './v284.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
