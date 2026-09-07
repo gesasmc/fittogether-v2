ee3b943ca3e348592f5c37207b3451dac57d56df
gesasmc <pz9v7b4rwr@privaterelay.appleid.com>
2026-09-07 10:28:15 +0200

    Add UI crash protection

--- src/main.jsx
+++ src/main.jsx
@@ -54,6 +54,7 @@ import './v297.js'
 import './v299.js'
 import App from './App.jsx'
 import AuthGate from './AuthGate.jsx'
+import StabilityBoundary from './StabilityBoundary.jsx'
 import './styles.css'
 import './v204.css'
 import './v205.css'
@@ -108,7 +109,8 @@ import './v295.css'
 import './v296.css'
 import './v297.css'
 import './v299.css'
+import './stability.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><AuthGate><App /></AuthGate></React.StrictMode>,
+  <React.StrictMode><StabilityBoundary><AuthGate><App /></AuthGate></StabilityBoundary></React.StrictMode>,
 )
