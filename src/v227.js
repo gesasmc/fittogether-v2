bdf48691e904b47c42d80a6fa0c8096dbbcc276a
gesasmc <pz9v7b4rwr@privaterelay.appleid.com>
2026-09-07 22:28:05 +0200

    Align cloud sync version marker

--- src/v227.js
+++ src/v227.js
@@ -1,6 +1,6 @@
 import { supabase, supabaseConfigured } from './lib/supabase.js'
 
-export const FITTOGETHER_VERSION = 'V2.0.110'
+export const FITTOGETHER_VERSION = 'V2.0.112'
 
 const fields = {
   profile: 'ft-profile', equipment: 'ft-equipment',
