d9975c7f7440ddb170b6e4037dbba2cd9a74e4a0
gesasmc <pz9v7b4rwr@privaterelay.appleid.com>
2026-09-09 20:51:01 +0200

    Load normalized training completion transform

--- vite302.config.js
+++ vite302.config.js
@@ -1,7 +1,8 @@
 import baseConfig from './vite.config.js'
 import cardioSearch302 from './vite302.js'
+import trainingCompletion303 from './vite303.js'
 
 export default {
   ...baseConfig,
-  plugins:[...(baseConfig.plugins||[]),cardioSearch302()],
+  plugins:[...(baseConfig.plugins||[]),cardioSearch302(),trainingCompletion303()],
 }
