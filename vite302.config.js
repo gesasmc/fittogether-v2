b3801370049c41531622ff7956e3b1fe1aae04cb
gesasmc <pz9v7b4rwr@privaterelay.appleid.com>
2026-09-09 22:00:26 +0200

    Remove fragile v303 completion transform

--- vite302.config.js
+++ vite302.config.js
@@ -1,8 +1,7 @@
 import baseConfig from './vite.config.js'
 import cardioSearch302 from './vite302.js'
-import trainingCompletion303 from './vite303.js'
 
 export default {
   ...baseConfig,
-  plugins:[...(baseConfig.plugins||[]),cardioSearch302(),trainingCompletion303()],
+  plugins:[...(baseConfig.plugins||[]),cardioSearch302()],
 }
