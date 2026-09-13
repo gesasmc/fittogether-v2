1b4b04fbf97bec6fbf53f1d1be1bb84e5d46eaa2
gesasmc <pz9v7b4rwr@privaterelay.appleid.com>
2026-09-13 19:23:17 +0200

    Load V2.0.154 weekly Smart Coach

--- src/main.jsx
+++ src/main.jsx
@@ -59,6 +59,7 @@ import './v314.js'
 import './v315.js'
 import './v317.js'
 import './v318.js'
+import './v319.js'
 import App from './App.jsx'
 import AuthGate from './AuthGate.jsx'
 import StabilityBoundary from './StabilityBoundary.jsx'
@@ -132,6 +133,7 @@ import './v314.css'
 import './v315.css'
 import './v317.css'
 import './v318.css'
+import './v319.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
