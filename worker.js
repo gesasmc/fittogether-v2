1eacf2fe2bfd77e503ba27e9263c67bd8c748d9a
gesasmc <pz9v7b4rwr@privaterelay.appleid.com>
2026-09-13 10:46:45 +0200

    Serve V2.0.145 shell through Worker with no-cache headers

---
+++ worker.js
@@ -0,0 +1,28 @@
+const VERSION='V2.0.145'
+
+const noStorePaths=new Set(['/', '/index.html', '/version.json', '/manifest.webmanifest'])
+
+export default {
+  async fetch(request, env) {
+    const url=new URL(request.url)
+    const response=await env.ASSETS.fetch(request)
+    const headers=new Headers(response.headers)
+
+    if(noStorePaths.has(url.pathname)){
+      headers.set('Cache-Control','no-store, no-cache, must-revalidate, max-age=0')
+      headers.set('Pragma','no-cache')
+      headers.set('Expires','0')
+      headers.set('X-FitTogether-Version',VERSION)
+    }
+
+    if(url.pathname==='/version.json'){
+      headers.set('Clear-Site-Data','"cache"')
+    }
+
+    return new Response(response.body,{
+      status:response.status,
+      statusText:response.statusText,
+      headers,
+    })
+  },
+}
