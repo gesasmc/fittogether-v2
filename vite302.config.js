450470f5ae3672a4306ac7fde7339bb7e0d3392e
gesasmc <pz9v7b4rwr@privaterelay.appleid.com>
2026-09-14 20:04:40 +0200

    Load cardio background timer fix

--- vite302.config.js
+++ vite302.config.js
@@ -1,7 +1,8 @@
 import baseConfig from './vite.config.js'
 import cardioSearch302 from './vite302.js'
+import cardioBackgroundFix325 from './vite325.js'
 
 export default {
   ...baseConfig,
-  plugins:[...(baseConfig.plugins||[]),cardioSearch302()],
+  plugins:[...(baseConfig.plugins||[]),cardioSearch302(),cardioBackgroundFix325()],
 }
