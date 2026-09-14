7780b9286c864f7599524553846ac8aee15ebd81
gesasmc <pz9v7b4rwr@privaterelay.appleid.com>
2026-09-14 20:22:12 +0200

    Load V2.0.162 cardio progress plugin

--- vite302.config.js
+++ vite302.config.js
@@ -1,8 +1,9 @@
 import baseConfig from './vite.config.js'
 import cardioSearch302 from './vite302.js'
 import cardioBackgroundFix325 from './vite325.js'
+import cardioProgress326 from './vite326.js'
 
 export default {
   ...baseConfig,
-  plugins:[...(baseConfig.plugins||[]),cardioSearch302(),cardioBackgroundFix325()],
+  plugins:[...(baseConfig.plugins||[]),cardioSearch302(),cardioBackgroundFix325(),cardioProgress326()],
 }
