8555b214c4cc9fb67509862b8e92451c0a46e295
gesasmc <pz9v7b4rwr@privaterelay.appleid.com>
2026-09-16 10:52:53 +0200

    Set Worker version V2.0.165

--- worker.js
+++ worker.js
@@ -1,4 +1,4 @@
-const VERSION='V2.0.164'
+const VERSION='V2.0.165'
 
 const noStorePaths=new Set(['/', '/index.html', '/version.json', '/manifest.webmanifest', '/ft-reminder-sw.js'])
 
@@ -7,22 +7,13 @@ export default {
     const url=new URL(request.url)
     const response=await env.ASSETS.fetch(request)
     const headers=new Headers(response.headers)
-
     if(noStorePaths.has(url.pathname)){
       headers.set('Cache-Control','no-store, no-cache, must-revalidate, max-age=0')
       headers.set('Pragma','no-cache')
       headers.set('Expires','0')
       headers.set('X-FitTogether-Version',VERSION)
     }
-
-    if(url.pathname==='/version.json'){
-      headers.set('Clear-Site-Data','"cache"')
-    }
-
-    return new Response(response.body,{
-      status:response.status,
-      statusText:response.statusText,
-      headers,
-    })
+    if(url.pathname==='/version.json')headers.set('Clear-Site-Data','"cache"')
+    return new Response(response.body,{status:response.status,statusText:response.statusText,headers})
   },
 }
