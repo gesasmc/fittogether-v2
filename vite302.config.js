b2546a3847b963e13331e384b154585c7b7542f0
gesasmc <pz9v7b4rwr@privaterelay.appleid.com>
2026-09-16 06:59:01 +0200

    Enable Smart Trainer day planner transform

--- vite302.config.js
+++ vite302.config.js
@@ -2,8 +2,9 @@ import baseConfig from './vite.config.js'
 import cardioSearch302 from './vite302.js'
 import cardioBackgroundFix325 from './vite325.js'
 import cardioProgress326 from './vite326.js'
+import smartDayPlanner328 from './vite328.js'
 
 export default {
   ...baseConfig,
-  plugins:[...(baseConfig.plugins||[]),cardioSearch302(),cardioBackgroundFix325(),cardioProgress326()],
+  plugins:[...(baseConfig.plugins||[]),cardioSearch302(),cardioBackgroundFix325(),cardioProgress326(),smartDayPlanner328()],
 }
